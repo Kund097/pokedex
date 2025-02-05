3d6299c07cf03a69b868d9854cd314e542ab88a0
Facundo <facund097dv@gmail.com>
2025-02-05 10:52:43 -0300

    primer commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+//Construir una pokedex https://pokeapi.co/
+
+//Consultar documentación https://pokeapi.co/docs/v2.html#
+//Listar pokemones, y poder cambiar de página
+//Ver detalles de 1 pokemon, incluyendo al menos 1 foto
+
